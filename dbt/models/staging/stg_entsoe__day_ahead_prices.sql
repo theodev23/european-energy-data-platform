0df608f2a34cff9ad88e6bcd8897510bc8639438
Théo Devarenne <devarennetheo@outlook.fr>
2026-08-22 14:57:17 +0200

    Add day-ahead prices staging model

---
+++ dbt/models/staging/stg_entsoe__day_ahead_prices.sql
@@ -0,0 +1,32 @@
+with source as (
+    select *
+    from {{ source('entsoe_raw', 'day_ahead_prices') }}
+),
+
+renamed as (
+    select
+        source_object_name,
+        document_mrid as document_id,
+        document_type,
+        revision_number,
+        document_created_at,
+        time_series_mrid as time_series_id,
+        auction_type,
+        business_type,
+        in_domain,
+        out_domain,
+        contract_market_agreement_type,
+        currency_unit,
+        price_unit,
+        curve_type,
+        period_start,
+        period_end,
+        resolution,
+        position,
+        point_timestamp,
+        price_amount
+    from source
+)
+
+select *
+from renamed
